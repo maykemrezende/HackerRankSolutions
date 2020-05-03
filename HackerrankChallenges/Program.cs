08c2535d04ec6fe6f203f5758ad1a28af18fd35b
Mayke Medeiros Rezende <maykemrezende@gmail.com>
2020-05-03 20:24:17 -0300

    primeiro commit

---
+++ HackerrankChallenges/Program.cs
@@ -0,0 +1,17 @@
+﻿using HackerrankChallenges.ChallengesSolutions;
+using System;
+using System.Collections.Generic;
+
+namespace HackerrankChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 1, 3, 2 };
+            var camelcaseQtd = Solutions.Birthday(list, 3, 2);
+            Console.WriteLine("camelcaseQtd: {0}", camelcaseQtd);
+            Console.ReadKey();
+        }
+    }
+}
