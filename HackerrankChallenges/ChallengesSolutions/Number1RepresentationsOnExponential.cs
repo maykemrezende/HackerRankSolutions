b37a8e318180b30f4c5a92aa1f9521d6a0a58fd8
Mayke Medeiros Rezende <maykemrezende@gmail.com>
2020-05-17 12:12:49 -0300

    Number1RepresentationsOnExponential solved

---
+++ HackerrankChallenges/ChallengesSolutions/Number1RepresentationsOnExponential.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HackerrankChallenges.ChallengesSolutions
+{
+    public static class Number1RepresentationsOnExponential
+    {
+        public static int Solution(int number)
+        {
+            if (number == 0) 
+                return 1;
+
+            long resultOfPow = 1;
+
+            for (int i = 1; i <= number; i++)
+                resultOfPow *= 11L;
+
+            Regex pattern = new Regex("1");
+            var mat = pattern.Matches(resultOfPow.ToString());
+
+            return mat.Count;
+        }
+    }
+}
