0d80f38949b5ecd1a51848c92dafe344d419ccaf
Mayke Medeiros Rezende <maykemrezende@gmail.com>
2020-05-17 13:04:25 -0300

    int number to roman number solved

---
+++ ChallengesTests/IntToRomanNumberTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using HackerrankChallenges.ChallengesSolutions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ChallengesTests
+{
+    public class IntToRomanNumberTests
+    {
+        [Theory]
+        [InlineData(20)]
+        public void TestIntToRomanNumber(int test1)
+        {
+            var result = IntToRomanNumber.ToRoman(test1);
+
+            result.Should().Be("XX");
+        }
+
+        [Theory]
+        [InlineData(3999)]
+        public void TestIntToRomanNumber1(int test1)
+        {
+            var result = IntToRomanNumber.ToRoman(test1);
+
+            result.Should().Be("MMMCMXCIX");
+        }
+    }
+}
