eb3f4d58281b35b19d2d21137337d004611f09b2
Mayke Medeiros Rezende <maykemrezende@gmail.com>
2020-05-17 12:41:51 -0300

    RegexForPathFolder solved for middle folder and last folder

---
+++ ChallengesTests/RegexForPathFolderTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using HackerrankChallenges.ChallengesSolutions;
+using Xunit;
+
+namespace ChallengesTests
+{
+    public class RegexForPathFolderTests
+    {
+        [Theory]
+        [InlineData("test/works/maybe")]
+        public void TestRegexForPathFolder(string test1)
+        {
+            var result = RegexForPathFolder.Solution(test1);
+
+            result.Should().Be("maybe");
+        }
+
+        [Theory]
+        [InlineData("")]
+        public void TestRegexForPathFolder1(string test1)
+        {
+            test1 = string.Empty;
+            var result = RegexForPathFolder.Solution(test1);
+
+            result.Should().Be(string.Empty);
+        }
+
+        [Theory]
+        [InlineData("folder/folder")]
+        public void TestRegexForPathFolder2(string test1)
+        {
+            test1 = string.Empty;
+            var result = RegexForPathFolder.Solution(test1);
+
+            result.Should().Be(string.Empty);
+        }
+
+        [Theory]
+        [InlineData("folder1/folder2/folder3")]
+        public void TestRegexForPathFolder3(string test1)
+        {
+            var result = RegexForPathFolder.SolutionForMiddleFolder(test1);
+
+            result.Should().Be("folder2");
+        }
+    }
+}
