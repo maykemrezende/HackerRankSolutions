d474da0fc579e699dedbb77130eb6f2ac895ae98
Mayke Medeiros Rezende <maykemrezende@gmail.com>
2020-05-18 20:30:26 -0300

    CompareLinkedLists solved

--- HackerrankChallenges/ChallengesSolutions/DataStructureSolutions.cs
+++ HackerrankChallenges/ChallengesSolutions/DataStructureSolutions.cs
@@ -191,5 +191,26 @@ namespace HackerrankChallenges.ChallengesSolutions
 
             return states;
         }
+
+        public static bool CompareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2)
+        {
+            if (head1 != null && head2 == null) return false;
+            if (head1 == null && head2 != null) return false;
+            if (head1 == null && head2 == null) return false;
+
+            while (head1 != null || head2 != null)
+            {
+                if (head1 != null && head2 == null) return false;
+                if (head1 == null && head2 != null) return false;
+
+                if (head1.data != head2.data)
+                    return false;
+
+                head1 = head1.next;
+                head2 = head2.next;
+            }
+
+            return true;
+        }
     }
 }
