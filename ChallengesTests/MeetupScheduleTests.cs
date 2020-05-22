b3a414ca2bd9e5c6401039913e9c66fd1427f26c
Mayke Medeiros Rezende <maykemrezende@gmail.com>
2020-05-22 00:53:30 -0300

    count meetings with count sort

---
+++ ChallengesTests/MeetupScheduleTests.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+using HackerrankChallenges.ChallengesSolutions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ChallengesTests
+{
+    public class MeetupScheduleTests
+    {
+        [Fact]
+        public void Testa()
+        {
+            var firstDay = new List<int>() { 3,1,10,11 };
+            var lastDay = new List<int>() { 3,11,10,11};
+
+            //for (int i = 0; i < 1739; i++)
+            //    firstDay.Add(new Faker().Random.Number());
+            //for (int i = 0; i < 1710; i++)
+            //    lastDay.Add(new Faker().Random.Number());
+
+            var teste = MeetupSchedule.countMeetings(firstDay, lastDay);
+        }
+    }
+
+    public class Teste
+    {
+        public List<int> FirstDay { get; set; }
+        public List<int> LastDay { get; set; }
+    }
+}
---
+++ HackerrankChallenges/ChallengesSolutions/MeetupSchedule.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerrankChallenges.ChallengesSolutions
+{
+    public class MeetupSchedule
+    {
+        public static int countMeetings(List<int> firstDay, List<int> lastDay)
+        {
+            if (firstDay.Count == 0 || lastDay.Count == 0) return 0;
+            if (firstDay.Count != lastDay.Count) return 0;
+
+            var investors = firstDay.Count;
+            var meetingsScheduled = new HashSet<int>();
+
+            var list = SortByPriority(firstDay, lastDay);
+
+            for (int investor = 0; investor < list.Count; investor++)
+            {
+                var indexInvestor = list[investor].investorIndex;
+                var firstDayOfInvestor = firstDay[indexInvestor];
+                var lastDayOfInvestor = lastDay[indexInvestor];
+
+                for (int day = firstDayOfInvestor; day <= lastDayOfInvestor; day++)
+                {
+                    if (!meetingsScheduled.Contains(day))
+                    {
+                        meetingsScheduled.Add(day);
+                        break;
+                    }
+                }                    
+            }            
+            
+            return meetingsScheduled.Count;
+        }
+
+        private static List<(int investorIndex, int daysAvailable)> SortByPriority(List<int> firstDay, List<int> lastDay)
+        {
+            var differencesCount = new int[lastDay.Max()];
+
+            for (int i = 0; i < firstDay.Count; i++)
+            {
+                int diff = Math.Abs(firstDay[i] - lastDay[i]);
+                differencesCount[diff]++;
+            }
+
+            for (int i = 1; i < differencesCount.Length; i++)            
+                differencesCount[i] = differencesCount[i] + differencesCount[i - 1];            
+
+            var investorsSortedByPriority = new List<(int, int)>();
+            investorsSortedByPriority.AddRange(Enumerable.Repeat((0,0), firstDay.Count));
+
+            for (int i = firstDay.Count - 1; i >= 0; i--)
+            {
+                int daysForInvestor = Math.Abs(firstDay[i] - lastDay[i]);
+                int position = differencesCount[daysForInvestor] - 1;
+                investorsSortedByPriority[position] = (i, daysForInvestor);
+
+                differencesCount[daysForInvestor]--;
+            }
+
+            return investorsSortedByPriority;
+        }
+    }
+}
